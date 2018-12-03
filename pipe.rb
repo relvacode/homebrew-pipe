4e832ff5f3f62c1f41d4d3617d5f62590d60d6ee
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-03 12:56:19 +0000

    Brew formula update for pipe version 0.0.6-alpha

--- pipe.rb
+++ pipe.rb
@@ -1,9 +1,9 @@
 class Pipe < Formula
   desc ""
   homepage ""
-  url "https://github.com/relvacode/pipe/releases/download/0.0.5-alpha/pipe_0.0.5-alpha_osx_x86_64.tar.gz"
-  version "0.0.5-alpha"
-  sha256 "dba674eb167517cfb52a6418f212ec3bbe26752f5518bf54c4edaa6d716da3af"
+  url "https://github.com/relvacode/pipe/releases/download/0.0.6-alpha/pipe_0.0.6-alpha_osx_x86_64.tar.gz"
+  version "0.0.6-alpha"
+  sha256 "99e1669e414cd578c4f33934b958e208acdd28d89c2b7117003810cf47ed70a0"
 
   def install
     bin.install "pipe"
